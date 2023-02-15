e0d205419c49e79a3dd8aeae1d4335514d1fa1e4
goeasyonng <meoa1026@gmail.com>
2023-02-15 19:25:02 +0900

    02-15

--- src/pages/QuestionPage.js
+++ src/pages/QuestionPage.js
@@ -1,105 +1,114 @@
-import React, { useEffect, useState } from 'react';
-import { questions } from '../data/QuizList';
-import { useNavigate } from 'react-router-dom';
-import '../CSS/question.css';
-import '../CSS/flipbook.css';
-import ProgressBar from '@ramonak/react-progress-bar';
+import React, { useEffect, useState } from "react";
+import { questions } from "../data/QuizList";
+import { useNavigate } from "react-router-dom";
+import "../CSS/question.css";
+import "../CSS/flipbook.css";
+import ProgressBar from "@ramonak/react-progress-bar";
 
 function Questions({ getData }) {
-   const [quizIndex, setQuizIndex] = useState(0);
-   const [endPage, setEndPage] = useState(false);
-   const [frontCount, setFrontCount] = useState(0);
-   const [backCount, setBackCount] = useState(0);
-   const [result, setResult] = useState([]);
-   const [kind, setKind] = useState('');
-   const navigate = useNavigate();
+  const [quizIndex, setQuizIndex] = useState(0);
+  const [endPage, setEndPage] = useState(false);
+  const [frontCount, setFrontCount] = useState(0);
+  const [backCount, setBackCount] = useState(0);
+  const [result, setResult] = useState([]);
+  const [kind, setKind] = useState("");
+  const navigate = useNavigate();
 
-   const onClick = option => {
-      if (option == 'front') {
-         setFrontCount(frontCount + 1);
-         setResult([...result, questions[quizIndex].answerOptions[0].resultText]);
+  const onClick = (option) => {
+    if (option == "front") {
+      setFrontCount(frontCount + 1);
+      setResult([...result, questions[quizIndex].answerOptions[0].resultText]);
+    } else {
+      setBackCount(backCount + 1);
+      setResult([...result, questions[quizIndex].answerOptions[1].resultText]);
+    }
+
+    const nextQuiz = quizIndex + 1;
+
+    if (nextQuiz < questions.length) {
+      setQuizIndex(nextQuiz);
+    } else {
+      setEndPage(true);
+
+      if (frontCount > backCount) {
+        setKind("당신에게 적합한 개발 영역은 프론트엔드입니다.");
+      } else if (frontCount < backCount) {
+        setKind("당신에게 적합한 개발 영역은 백엔드입니다.");
       } else {
-         setBackCount(backCount + 1);
-         setResult([...result, questions[quizIndex].answerOptions[1].resultText]);
+        setKind("당신에게 적합한 개발 영역은 풀스택입니다.");
       }
+    }
+  };
 
-      const nextQuiz = quizIndex + 1;
+  useEffect(() => {
+    getData(kind, result);
+  }, [kind]);
 
-      if (nextQuiz < questions.length) {
-         setQuizIndex(nextQuiz);
-      } else {
-         setEndPage(true);
+  useEffect(() => {
+    if (kind && endPage) {
+      navigate(`/Result`);
+    }
+  }, [kind, endPage]);
 
-         if (frontCount > backCount) {
-            setKind('당신에게 적합한 개발 영역은 프론트엔드입니다.');
-         } else if (frontCount < backCount) {
-            setKind('당신에게 적합한 개발 영역은 백엔드입니다.');
-         } else {
-            setKind('당신에게 적합한 개발 영역은 풀스택입니다.');
-         }
-      }
-   };
+  console.log("frontCount:", frontCount);
+  console.log("backCount:", backCount);
 
-   useEffect(() => {
-      getData(kind, result);
-   }, [kind]);
+  return (
+    <>
+      <div className="book">
+        {/* // 애니메이션만 적용! */}
+        <div className="bookList">
+          <div className="coverFront"></div>
+          <div className="page"></div>
+          <div className="page"></div>
+          <div className="page"></div>
+          <div className="page"></div>
+          <div className="page"></div>
+          {/* 애니메이션 끝 */}
 
-   useEffect(() => {
-      if (kind && endPage) {
-         navigate(`/Result`);
-      }
-   }, [kind, endPage]);
-
-   console.log('frontCount:', frontCount);
-   console.log('backCount:', backCount);
-
-   return (
-      <>
-         <div className='book'>
-            {/* // 애니메이션만 적용! */}
-            <div className='bookList'>
-               <div className='coverFront'></div>
-               <div className='page'></div>
-               <div className='page'></div>
-               <div className='page'></div>
-               <div className='page'></div>
-               <div className='page'></div>
-               {/* 애니메이션 끝 */}
-
-               {/* 컴포넌트 데이터 전달 */}
-               <div className='imagePage'>
-                  <img className='imgs' src={questions[quizIndex].url} />
-               </div>
-               <div className='lastPage'>
-                  <div className='rightPage'>
-                     <div className='progress'>
-                        <ProgressBar completed={Math.floor((quizIndex / questions.length) * 100)} bgColor='#fff' baseBgColor='#2d2d2e' width='300px' />
-                     </div>
-                     <div className='book__tex__page'>
-                        {endPage ? (
-                           <></>
-                        ) : (
-                           <div className='question__page'>
-                              <div className='question__title'>
-                                 <span>{questions[quizIndex].questionText}</span>
-                              </div>
-                              <div className='question__text'>
-                                 {questions[quizIndex].answerOptions.map(item => (
-                                    <button className='question__btn' onClick={() => onClick(item.option)}>
-                                       {item.answerText}
-                                    </button>
-                                 ))}
-                              </div>
-                           </div>
-                        )}
-                     </div>
+          {/* 컴포넌트 데이터 전달 */}
+          <div className="imagePage">
+            <img className="imgs" src={questions[quizIndex].url} />
+          </div>
+          <div className="lastPage">
+            <div className="rightPage">
+              <div>
+                <ProgressBar
+                  className="progress"
+                  completed={Math.floor((quizIndex / questions.length) * 100)}
+                  bgColor="#fff"
+                  baseBgColor="#2d2d2e"
+                  width="300px"
+                />
+              </div>
+              <div className="book__tex__page">
+                {endPage ? (
+                  <></>
+                ) : (
+                  <div className="question__page">
+                    <div className="question__title">
+                      <span>{questions[quizIndex].questionText}</span>
+                    </div>
+                    <div className="question__text">
+                      {questions[quizIndex].answerOptions.map((item) => (
+                        <button
+                          className="question__btn"
+                          onClick={() => onClick(item.option)}
+                        >
+                          {item.answerText}
+                        </button>
+                      ))}
+                    </div>
                   </div>
-               </div>
-               <div className='coverBack'></div>
+                )}
+              </div>
             </div>
-         </div>
-      </>
-   );
+          </div>
+          <div className="coverBack"></div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Questions;
