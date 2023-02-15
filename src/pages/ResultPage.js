babefa4fab42a0a4e09ab6389d21ebe56c06ceda
goeasyonng <meoa1026@gmail.com>
2023-02-15 10:32:42 +0900

    02-15

--- src/pages/ResultPage.js
+++ src/pages/ResultPage.js
@@ -1,98 +1,14 @@
-import React, { useState } from 'react';
-import { questions } from '../data/QuizList';
-import '../CSS/index.css';
+import React from "react";
 
-function ResultPage() {
-   const [quizIndex, setQuizIndex] = useState(0); //현재 문제 페이지를 세는 변수
-   const [endPage, setEndPage] = useState(false); //문제 페이지의 끝에서 True로 바뀌게 하는 변수
-   const [frontCount, setFrontCount] = useState(0); //선택지 1을 누른 횟수를 세는 변수
-   const [backCount, setBackCount] = useState(0); //선택지 2를 누른 횟수를 세는 변수
-   const [result, setResult] = useState([]); //선택에 따라 해당 선택에 따른 각각 다른 문장을 담아두는 변수
-
-   //버튼 클릭 이벤트가 발생했을 떄 시행되는 훅
-   const onClick = option => {
-      //선택에 따른 횟수와 결과문장을 담아두는 함수
-      if (option == 'front') {
-         setFrontCount(frontCount + 1);
-         setResult([...result, questions[quizIndex].answerOptions[0].resultText]);
-      } else {
-         setBackCount(backCount + 1);
-         setResult([...result, questions[quizIndex].answerOptions[1].resultText]);
-      }
-
-      //총 페이지를 세어 마지막을 나타내는 함수
-      const nextQuiz = quizIndex + 1;
-
-      if (nextQuiz < questions.length) {
-         setQuizIndex(nextQuiz);
-      } else {
-         setEndPage(true);
-      }
-   };
-
-   console.log('frontCount:', frontCount);
-   console.log('backCount:', backCount);
-   console.log(
-      'result',
-      result.map(item => <div>{item}</div>),
-   );
-
-   return (
-      <>
-         <div className='book__text page'>
-            {endPage ? (
-               <section>
-                  {frontCount >= backCount ? (
-                     frontCount > backCount ? (
-                        <div>
-                           <div>당신에게 적합한 개발 영역은 프론트엔드입니다.</div>
-                           <div>
-                              {result.map(item => (
-                                 <div>{item}</div>
-                              ))}
-                           </div>
-                        </div>
-                     ) : (
-                        <div>
-                           <div>당신에게 적합한 개발 영역은 풀스택입니다.</div>
-                           <div>
-                              {result.map(item => (
-                                 <div>{item}</div>
-                              ))}
-                           </div>
-                        </div>
-                     )
-                  ) : (
-                     <div>
-                        <div>당신에게 적합한 개발 영역은 백엔드입니다.</div>
-                        <div>
-                           {result.map(item => (
-                              <div>{item}</div>
-                           ))}
-                        </div>
-                     </div>
-                  )}
-               </section>
-            ) : (
-               <section className='text__page'>
-                  <div>
-                     {/* 페이지 위치 */}
-                     {/* <p>
-                {quizIndex + 1}/ {questions.length}
-              </p> */}
-                     <span>{questions[quizIndex].questionText}</span>
-                  </div>
-                  {/* 선택 */}
-                  <div>
-                     {questions[quizIndex].answerOptions.map(item => (
-                        <button onClick={() => onClick(item.option)}>{item.answerText}</button>
-                     ))}
-                  </div>
-               </section>
-            )}
-         </div>
-      </>
-   );
+export default function Result({ title, detail }) {
+  return (
+    <div>
+      <div>{title} </div>
+      <div>
+        {detail.map((item) => (
+          <div>{item}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
-
-export default ResultPage;
--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -1,32 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // request react-router-dom librasy install
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Start() {
-   const handleClick = e => {
-      const target = e.target.value;
-      console.log(target);
-   };
+  return (
+    <div className="book__">
+      <div className="book__cover page">
+        <h1 className="content__title">DEVELOPER OF OZ</h1>
+        <div className="book__typewriter">
+          <p>오즈와 마법사로 알아보는 개발자 성향 테스트</p>
+        </div>
 
-   return (
-      <div className='book__'>
-         <div className='book__cover page'>
-            <h1 className='content__title'>DEVELOPER OF OZ</h1>
-            <div className='book__typewriter'>
-               <p>오즈와 마법사로 알아보는 개발자 성향 테스트</p>
-            </div>
+        <Link className="book__guide" to="/Question">
+          <button type="button">클릭하기!</button>
+        </Link>
 
-            <Link className='book__guide' to='/Question'>
-               <button type='button' onClick={handleClick}>
-                  클릭하기!
-               </button>
-            </Link>
-
-            <div className='book__cover page'>
-               <span>OZ 코딩스쿨</span>
-            </div>
-         </div>
+        <div className="book__cover page">
+          <span>OZ 코딩스쿨</span>
+        </div>
       </div>
-   );
+    </div>
+  );
 }
 
 export default Start;
