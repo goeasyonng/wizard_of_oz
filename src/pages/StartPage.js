75763c707c81de9d6c3c07c71f82cec6b87dbfcc
changseob <ggggjkas@gmail.com>
2023-02-15 17:23:33 +0900

    1. 웹 - 책펼처짐 과 질문 항목 css 완료 2. 질물항목에 이미지 업로드 및 css 완료

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -1,41 +1,37 @@
-import React from "react";
-import { Link } from "react-router-dom"; // request react-router-dom librasy install
+import React from 'react';
+import { Link } from 'react-router-dom'; // request react-router-dom librasy install
+import '../CSS/start.css';
 
 export default function BookQuiz() {
-  const handleClick = (e) => {
-    const target = e.target.value;
-    console.log(target);
-  };
+   const handleClick = e => {
+      const target = e.target.value;
+      console.log(target);
+   };
 
-  return (
-    <div className="book">
-      <div className="book__coverpage">
-        <h1 className="content__title">
-          DEVELOPER <span className="of">OF</span>{" "}
-          <span className="oz">OZ</span>
-        </h1>
-        <div className="book__typewriter">
-          <p className="start_info">오즈의 마법같은 개발자 성향 테스트 !!</p>
-        </div>
+   return (
+      <div className='book'>
+         <div className='book__coverpage'>
+            <h1 className='content__title'>
+               DEVELOPER <span className='of'>OF</span> <span className='oz'>OZ</span>
+            </h1>
+            <div className='book__typewriter'>
+               <p className='start_info'>오즈의 마법같은 개발자 성향 테스트 !!</p>
+            </div>
 
-        <Link className="book__guide" to="/Question">
-          <button className="start_button" onClick={handleClick}>
-            시작하기
-          </button>
-        </Link>
+            <Link className='book__guide' to='/Question'>
+               <button className='start_button' onClick={handleClick}>
+                  시작하기
+               </button>
+            </Link>
 
-        <div className="brand_logo">
-          <a href="https://ozcodingschool.com/" target="_blank">
-            <button className="oz_logo">
-              <img
-                src={`${process.env.PUBLIC_URL}/ozcoding_logo.png`}
-                className="App-logo"
-                alt="React"
-              />
-            </button>
-          </a>
-        </div>
+            <div className='brand_logo'>
+               <a href='https://ozcodingschool.com/' target='_blank'>
+                  <button className='oz_logo'>
+                     <img src={`${process.env.PUBLIC_URL}/ozcoding_logo.png`} className='App-logo' alt='React' />
+                  </button>
+               </a>
+            </div>
+         </div>
       </div>
-    </div>
-  );
+   );
 }
